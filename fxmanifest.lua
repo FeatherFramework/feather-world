c53e13f5ab4d9f59882c2328103cd5f0f89c4848
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-09-23 19:12:16 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-world'
 description 'World population and map compatibility runtime for Feather Framework'
 author 'Feather Framework'
-version '0.3.0'
+version '0.2.0'
 
 shared_script 'config.lua'
 
